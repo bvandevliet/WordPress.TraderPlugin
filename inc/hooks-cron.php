7756d047b8c8cc8266ccb71bef679c7a0942a553
Bob Vandevliet <info@bvandevliet.nl>
2021-11-19 08:02:40 +0100

    maybe more robust

--- inc/hooks-cron.php
+++ inc/hooks-cron.php
@@ -18,7 +18,7 @@ add_action(
     /**
      * Bail if event was not triggered by the system task scheduler.
      */
-    if ( ! empty( get_option( 'trader_disable_wp_cron', false ) ) && ( $_SERVER['REMOTE_ADDR'] ?? 0 ) !== ( $_SERVER['SERVER_ADDR'] ?? 1 ) ) {
+    if ( ! empty( get_option( 'trader_disable_wp_cron', false ) ) && ( $_SERVER['REMOTE_ADDR'] ?? '0' ) !== ( $_SERVER['SERVER_ADDR'] ?? -1 ) ) {
       return;
     }
 
