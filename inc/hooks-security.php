008e9eabf51efc157da3984aa71bcb2235152d23
Bob Vandevliet <info@bvandevliet.nl>
2022-03-20 15:51:38 +0100

    removed redundant ajax security hook

--- inc/hooks-security.php
+++ inc/hooks-security.php
@@ -21,42 +21,6 @@ add_action(
 
 
 /**
- * Enhanced security for sensitive AJAX requests.
- */
-add_action(
-  'check_ajax_referer',
-  /**
-   * Fires once the Ajax request has been validated or not.
-   *
-   * @param string    $action The Ajax nonce action.
-   * @param false|int $result False if the nonce is invalid, 1 if the nonce is valid and generated between
-   *                          0-12 hours ago, 2 if the nonce is valid and generated between 12-24 hours ago.
-   *
-   * @link https://developer.wordpress.org/reference/functions/check_ajax_referer/
-   */
-  function ( $action, $result )
-  {
-    if ( in_array(
-      $action,
-      array(
-        'ajax_example',
-      ),
-      true
-    ) ) {
-      if ( ! is_user_logged_in() || 1 !== $result ) {
-        if ( wp_doing_ajax() ) {
-          wp_die( -1, 403 );
-        }
-        die( '-1' );
-      }
-    }
-  },
-  PHP_INT_MAX,
-  2
-);
-
-
-/**
  * Re-define nocache headers.
  */
 add_filter(
